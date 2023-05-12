e27b781e0ceae974230b7b11d824b341bb5c70fd
sbslayos <german.martinez.mt@usa.edu.co>
2023-05-12 03:13:01 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <section>
+            <h1>Header</h1>
+        </section>
+    </div>
+  )
+}
+
+export default Header;

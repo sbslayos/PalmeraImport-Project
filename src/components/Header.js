67b13f1a5f035bef6f66fd691474297c74edb9ae
sbslayos <german.martinez.mt@usa.edu.co>
2023-05-19 00:13:16 -0500

    19/05

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
+    
     <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex bg-white py-4">
+      <div className="md:flex bg-teal-300 py-0 ">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800" >
-          
-      <h2 className="text-red-700" >Palmera Import</h2>
-    <nav>
+        
+        <span className="text-3xl text-indigo-600 mr-1 pt-2" >
+        <img src="/img/1-removebg-preview.png" className="max-w-[80px] h-[80px] " />
+        <h3>Palmera Import</h3>
+      </span>
       
+      
+
+    <nav>
       <Link to="/">Inicio</Link>
       <Link to="/about">Sobre Nosotros</Link>
       <Link to="/login">Ingresar</Link>
